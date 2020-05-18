30e9af453d95fcd007e9f7a321640bce3e92761b
Maria Beatriz <beatrizflorenccio@gmail.com>
2020-05-18 20:23:31 -0300

    Create teste_led.ino

---
+++ teste_led/teste_led.ino
@@ -0,0 +1,16 @@
+#define LED_BUILTIN 2
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(300);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(300);
+  digitalWrite(LED_BUILTIN, HIGH);
+
+}
