dd4c1c65641864516398a96c0cba0f901a743e20
Maria Beatriz <beatrizflorenccio@gmail.com>
2020-12-04 03:38:52 -0300

    Update basico.ino

--- mdash/basico/basico.ino
+++ mdash/basico/basico.ino
@@ -1,16 +1,24 @@
+/* CÓDIGO BÁSICO CONEXÃO ESP32 MDASH
+   
+   REFERÊNCIA: https://mdash.net/docs/#arduino-ide
+*/
+
 #include <mDash.h>
 #include <WiFi.h>
 
-#define WIFI_NETWORK "MJVE" // SSID wifi
-#define WIFI_PASSWORD "pituca2019" // Senha wifi
-#define DEVICE_PASSWORD "2nvgZUeF0Sum74JyQRWUJQ" // Token do dispositivo no mDash
+
+#define WIFI_NETWORK "SSID" // SSID wifi
+#define WIFI_PASSWORD "PASSWORD" // Senha wifi
+#define DEVICE_PASSWORD "TOKEN" // Token do dispositivo no mDash
 #define MDASH_APP_NAME "MinimalApp"
+
 
 void setup() {
   Serial.begin(115200);
   WiFi.begin(WIFI_NETWORK, WIFI_PASSWORD);
   mDashBegin(DEVICE_PASSWORD);
 }
+
 
 void loop() {
   delay(100);
